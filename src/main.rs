ba50ae950b114de2311b5f22ed84b361d3b2af38
Steve Canham <stevecanham@outlook.com>
2025-02-05 18:50:59 +0000

    Initial assembly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+
+use imp_geo::error_defs::AppError;
+use imp_geo::run;
+use std::env;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), AppError> {
+
+    let args: Vec<_> = env::args_os().collect();
+    run(args).await
+    
 }
